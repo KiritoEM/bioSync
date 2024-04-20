609edbef3f3886ce41921834a01e03ef195344df
kiritoEM <johankirito64@gmail.com>
2024-04-20 08:04:57 +0200

    add responsive-gap

---
+++ src/components/sections/landing/landingCommunity.tsx
@@ -0,0 +1,9 @@
+const landingCommunity = () : JSX.Element => {
+    return (
+        <section className="landing__community">
+            
+        </section>
+    );
+};
+
+export default landingCommunity;
