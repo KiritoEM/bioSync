ee9bc101334721eca8a152359332741681f17340
kiritoEM <johankirito64@gmail.com>
2024-04-20 07:15:43 +0200

    add content to features-landing

--- src/components/sections/landing/LandingFeatures.tsx
+++ src/components/sections/landing/LandingFeatures.tsx
@@ -3,7 +3,11 @@ const LandingFeatures = (): JSX.Element => {
     <section id="landing-features">
       <div className="row">
         <div className="col-6">
-          <div className="features-content"></div>
+          <div className="features-content">
+            <div className="title">
+              <h2>Discover our features</h2>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <div className="features-image">
