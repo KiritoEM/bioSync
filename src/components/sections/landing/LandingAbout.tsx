aff1d656914605dc782cd81dac9add9beea32f4c
kiritoEM <johankirito64@gmail.com>
2024-04-20 14:36:58 +0200

    add responsive to landing

--- src/components/sections/landing/LandingAbout.tsx
+++ src/components/sections/landing/LandingAbout.tsx
@@ -8,14 +8,10 @@ const LandingAbout = (): JSX.Element => {
         <h2>How our app Work</h2>
       </div>
       <div className="about-content">
-        <div className="row gx-5 gy-5 gy-md-2">
+        <div className="row gx-5 gy-5 gy-lg-2">
           {aboutCardData.map((item, index) => (
-            <div className="col-md-4" key={index}>
-              <LandingCard
-                {...item}
-                align="center"
-                card_height="13rem"
-              />
+            <div className="col-md-6 col-lg-4" key={index}>
+              <LandingCard {...item} align="center" card_height="13rem" />
             </div>
           ))}
         </div>
--- src/components/sections/landing/LandingCommunity.tsx
+++ src/components/sections/landing/LandingCommunity.tsx
@@ -2,12 +2,12 @@ const LandingCommunity = (): JSX.Element => {
   return (
     <section className="landing__community">
       <div className="row gy-4">
-        <div className="col-md-7">
+        <div className="col-md-6 col-lg-7">
           <div className="community-header">
             <h1>Join the community</h1>
           </div>
         </div>
-        <div className="col-md-5">
+        <div className="col-md-6 col-lg-5">
           <div className="community-subscribe">
             <h5>Subscribe to the newsletter</h5>
             <div className="input">
